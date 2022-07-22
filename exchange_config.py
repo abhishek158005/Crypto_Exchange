08aa1f4acc6314f9862023c0f3f89507e1f130e0
abhishek jain <jainabhishek347@gmail.com>
2022-07-22 19:36:37 +0530

    Update exchange_config.py

--- exchange_config.py
+++ exchange_config.py
@@ -1,10 +1,10 @@
 api ={ 
-    'api_binance' : 'jsjSFM96Vfb8j0pVhI95pBHAgaPaxlIUGiymNILBLdf47QhjWnfwDJyh8NBVionz',
-    'api_wazirx' : '82qirCBzABBkcN31y4DhFHgNSj6O32tzWCpDntbF7o72UMjfJJmtOY6Q9TNwIeNo'
+    'api_binance' : '',
+    'api_wazirx' : ''
     
     }
 
 secret = {
-    'secret_binance' : 'pixedTBRV7SouXrBlD2ui3ZvR9KkxWhcTmODz2qoPQ7qwtHJpmsD68AkkMlhaD8I',
-    'secret_wazirx' : '5rDkdK2zXnyFOFLgXfJNJ7sVWCfXtsUIXAFfXxxw'
+    'secret_binance' : '',
+    'secret_wazirx' : ''
 }
